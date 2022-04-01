8e3c94946b9d0a962ca2a57a9e6a927b2ada8ceb
Ken Friis Larsen <ken@friislarsen.net>
2022-04-01 15:49:02 +0200

    Initial commit

---
+++ FscWrap.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+open FSharp.Compiler.CodeAnalysis
+
+// Create an interactive checker instance
+let checker = FSharpChecker.Create()
+
+[<EntryPoint>]
+let main args =
+    printfn "Arguments passed to fsc-wrap : %A" args
+    let errors, exitCode =
+        Array.append [| "fsc.exe" |] args
+        |> checker.Compile
+        |> Async.RunSynchronously
+    printfn "Compiler errors %A" errors
+    printfn "Compiler exit code was %i" exitCode
+    exitCode
---
+++ README.md
@@ -0,0 +1,6 @@
+A wrapper for the F# compiler `fsc`
+===================================
+
+Are you missing being able to run the F# compiler `fsc` (aka
+`fsharpc`) after moring to `dotnet` from `mono`.  This project is for
+you.
